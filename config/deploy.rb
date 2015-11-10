f1a7444cc9fc4269164d19952b1147d76f2a5c6a
seairy <seairy@gmail.com>
2015-11-10 13:34:30 +0800

    init commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@github.com:zzch/Alwoodley.git'
 
 set :keep_releases, 3
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/system}
 
 set :use_sudo, false
