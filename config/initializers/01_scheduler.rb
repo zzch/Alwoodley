d8a321ef6d1dfad2c4447d8c5399ba17ab65585b
seairy <seairy@gmail.com>
2015-11-10 13:25:17 +0800

    init commit

--- config/initializers/01_scheduler.rb
+++ config/initializers/01_scheduler.rb
@@ -2,7 +2,7 @@
 require 'rufus-scheduler'
 
 Rufus::Scheduler.new.every '30s' do
-  uri = URI.parse("http://staging.lianqiubao.com/api/v1/managements/update_weather")
+  uri = URI.parse('http://staging.lianqiubao.com/api/v1/managements/update_weather')
   http = Net::HTTP.new(uri.host, uri.port)
   request = Net::HTTP::Put.new(uri.request_uri)
 end
