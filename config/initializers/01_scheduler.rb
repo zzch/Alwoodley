46dbdb1d8e9b5fff87a0bef4db0e88e5b5ddc63a
seairy <seairy@gmail.com>
2015-11-16 10:34:23 +0800

    init commit

--- config/initializers/01_scheduler.rb
+++ config/initializers/01_scheduler.rb
@@ -3,7 +3,7 @@ require 'rufus-scheduler'
 
 scheduler = Rufus::Scheduler.new
 
-scheduler.every '1h' do
+scheduler.every '10m' do
   uri = URI.parse('http://staging.lianqiubao.com/api/v1/managements/update_weather')
   http = Net::HTTP.new(uri.host, uri.port)
   request = Net::HTTP::Put.new(uri.request_uri)
