67e6f54722ddee853aa84411b435844b51c6a505
seairy <seairy@gmail.com>
2015-12-10 13:37:20 +0800

    init commit

--- config/initializers/01_scheduler.rb
+++ config/initializers/01_scheduler.rb
@@ -15,3 +15,16 @@ scheduler.every '10m' do
     SchedulerLog.create!(result: 'failure')
   end
 end
+
+scheduler.every '10m' do
+  begin
+    uri = URI.parse('http://lianqiubao.com/api/v1/managements/update_weather')
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Put.new(uri.request_uri)
+    response = http.request(request)
+    json = JSON.parse(response.body)
+    SchedulerLog.create!(result: e.message)
+  rescue StandardException => e
+    SchedulerLog.create!(result: 'failure')
+  end
+end
